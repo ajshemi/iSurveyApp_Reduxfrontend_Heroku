6441c7eabdb093134f334ef83eeb002c6320453f
ajshemi <akpojotor@yahoo.com>
2020-07-28 01:41:01 -0400

    updated navbar and index.css

--- src/App.js
+++ src/App.js
@@ -1,80 +1,82 @@
-import React from 'react';
-import {Switch, Route} from 'react-router'
+import React from "react";
+import { Switch, Route } from "react-router";
 
-import {withRouter} from 'react-router-dom'
-import { Container } from 'semantic-ui-react'
+import { withRouter } from "react-router-dom";
+import { Container } from "semantic-ui-react";
 
-import Form from './components/Form'
-import NavBar from './components/NavBar'
-import Header from './components/Header'
-import Home from './components/Home'
-import ProductContainer from './components/ProductContainer'
-import AllCommentsContainer from './components/AllCommentsContainer'
+import Form from "./components/Form";
+import NavBar from "./components/NavBar";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import ProductContainer from "./components/ProductContainer";
+import AllCommentsContainer from "./components/AllCommentsContainer";
 // import UserRatingContainer from './userRatingComponents/UserRatingContainer'
 // import RatingContainer from './userRatingComponents/RatingContainer'
 // import NewRatingContainer from './userRatingComponents/NewRatingContainer'
-import NewUserRatingContainer from './userRatingComponents/NewUserRatingContainer'
-// import WatsonSentimentContainer from './watsoncomponents/WatsonSentimentContainer' 
+import NewUserRatingContainer from "./userRatingComponents/NewUserRatingContainer";
+// import WatsonSentimentContainer from './watsoncomponents/WatsonSentimentContainer'
 // import WatsonEmotionContainer from './watsoncomponents/WatsonEmotionContainer'
-import AnalysisContainer from './components/AnalysisContainer'
-import RatingSummaryContainer from './components/RatingSummaryContainer'
+import AnalysisContainer from "./components/AnalysisContainer";
+import RatingSummaryContainer from "./components/RatingSummaryContainer";
 
-import ChartsContainer from './components/ChartsContainer'   
-import PageNotFound from './components/PageNotFound' 
+import ChartsContainer from "./components/ChartsContainer";
+import PageNotFound from "./components/PageNotFound";
 
-
-import {connect} from 'react-redux'
-import {clearallEmotionsFromState,clearSentimentFromState,clearUserFromState,addProductsToState,saveUserToState,addAllCommentsToState,addAllSentimentsToState,addAllEmotionsToState} from './Redux/actions'
-
-
+import { connect } from "react-redux";
+import {
+  clearallEmotionsFromState,
+  clearSentimentFromState,
+  clearUserFromState,
+  addProductsToState,
+  saveUserToState,
+  addAllCommentsToState,
+  addAllSentimentsToState,
+  addAllEmotionsToState,
+} from "./Redux/actions";
 
 class App extends React.Component {
-
   componentDidMount() {
     //render products and comments when app loads
     fetch("https://cookiesurveywebapp.herokuapp.com/products")
-    .then(r => r.json())
-    .then((products) => {
-      // console.log(products)
-      this.props.addProductsToState(products);
-    })   
-    
-  
+      .then((r) => r.json())
+      .then((products) => {
+        // console.log(products)
+        this.props.addProductsToState(products);
+      });
+
     fetch("https://cookiesurveywebapp.herokuapp.com/comments")
-    .then(r => r.json())
-    .then((allcomments) => {
-      this.props.addAllCommentsToState(allcomments) 
-    })
+      .then((r) => r.json())
+      .then((allcomments) => {
+        this.props.addAllCommentsToState(allcomments);
+      });
 
     fetch("https://cookiesurveywebapp.herokuapp.com/watson_sentiments")
-    .then(r => r.json())
-    .then((allsentiments) => {
-      this.props.addAllSentimentsToState(allsentiments) 
-    })
+      .then((r) => r.json())
+      .then((allsentiments) => {
+        this.props.addAllSentimentsToState(allsentiments);
+      });
 
     fetch("https://cookiesurveywebapp.herokuapp.com/watson_emotions")
-    .then(r => r.json())
-    .then((allemotions) => {
-      this.props.addAllEmotionsToState(allemotions) 
-    })
+      .then((r) => r.json())
+      .then((allemotions) => {
+        this.props.addAllEmotionsToState(allemotions);
+      });
 
     //persist when token exist
     if (localStorage.getItem("token")) {
-      let token = localStorage.getItem('token')
+      let token = localStorage.getItem("token");
       fetch("https://cookiesurveywebapp.herokuapp.com/persist", {
         headers: {
-          "Authorization": `bearer ${token}`
-        }
+          Authorization: `bearer ${token}`,
+        },
       })
-      .then(r => r.json())
-      .then(resp => {
-        if (resp.token) {
-          this.props.saveUserToState(resp);
-        }
-      })
+        .then((r) => r.json())
+        .then((resp) => {
+          if (resp.token) {
+            this.props.saveUserToState(resp);
+          }
+        });
     }
-
-
   }
 
   handleLoginSubmit = (user) => {
@@ -83,100 +85,99 @@ class App extends React.Component {
       method: "POST",
       body: JSON.stringify(user),
       headers: {
-        "content-type": "application/json"
-      }
+        "content-type": "application/json",
+      },
     })
-      .then(r => r.json())
+      .then((r) => r.json())
       .then((resp) => {
         if (resp.token) {
           this.props.saveUserToState(resp);
-          localStorage.setItem("token", resp.token)
-          this.props.history.push("/products")
+          localStorage.setItem("token", resp.token);
+          this.props.history.push("/products");
         }
-      })
+      });
 
     // this.refreshPage()
-
-  }
+  };
 
   handleRegisterSubmit = (user) => {
     //
     fetch("https://cookiesurveywebapp.herokuapp.com/users", {
       method: "POST",
       headers: {
-        "content-type": "application/json"
+        "content-type": "application/json",
       },
-      body: JSON.stringify(
-        user
-      )
+      body: JSON.stringify(user),
     })
-    .then(r => r.json())
-    .then(resp=> {
-      if (resp.token) {
-        // console.log(resp)
-        this.props.saveUserToState(resp);
-        localStorage.setItem("token", resp.token)
-        this.props.history.push("/products")
-      }
-    })
+      .then((r) => r.json())
+      .then((resp) => {
+        if (resp.token) {
+          // console.log(resp)
+          this.props.saveUserToState(resp);
+          localStorage.setItem("token", resp.token);
+          this.props.history.push("/products");
+        }
+      });
     // this.refreshPage()
-  }
+  };
   renderForm = (routerProps) => {
-    if(routerProps.location.pathname === "/login"){
-      return <Form formName="Login Form" handleSubmit={this.handleLoginSubmit}/>
+    if (routerProps.location.pathname === "/login") {
+      return (
+        <Form formName="Login Form" handleSubmit={this.handleLoginSubmit} />
+      );
     } else if (routerProps.location.pathname === "/signup") {
-      return <Form formName="SignUp Form" handleSubmit={this.handleRegisterSubmit}/>
+      return (
+        <Form formName="SignUp Form" handleSubmit={this.handleRegisterSubmit} />
+      );
     }
-  }
+  };
   // refreshPage=() =>{
   //   window.location.reload(false);
   // }
-  
-  handleLogout=(routerProps) => {
-    if(routerProps.location.pathname === "/logout"){
-    localStorage.clear()
-    this.props.clearUserFromState()
-    // this.props.clearProductsFromState()
-    // this.props.clearallCommentsFromState()
-    // this.props.clearSentimentFromState() 
-    // this.props.clearallEmotionsFromState()
-   
-    return ( <Home /> )
+
+  handleLogout = (routerProps) => {
+    if (routerProps.location.pathname === "/logout") {
+      localStorage.clear();
+      this.props.clearUserFromState();
+      // this.props.clearProductsFromState()
+      // this.props.clearallCommentsFromState()
+      // this.props.clearSentimentFromState()
+      // this.props.clearallEmotionsFromState()
+
+      return <Home />;
     }
-  }
+  };
 
-
-  render(){
+  render() {
     // console.log(this.props);
     return (
       <Container>
         {/* <div className="App"> */}
-          <Header/>
-          <NavBar/>
-          <Switch>
-            <Route path="/login" render={ this.renderForm } />
-            <Route path="/signup" render={ this.renderForm } />
-            <Route path="/products" component={ ProductContainer } />
-            {/* <Route path="/rating" component={ UserRatingContainer } /> */}
-            {/* <Route path="/rating" component={ NewRatingContainer } />  */}
-            <Route path="/newrating" component={ NewUserRatingContainer } />
+        {/* <Header/> */}
+        <NavBar />
+        <Switch>
+          <Route path="/login" render={this.renderForm} />
+          <Route path="/signup" render={this.renderForm} />
+          <Route path="/products" component={ProductContainer} />
+          {/* <Route path="/rating" component={ UserRatingContainer } /> */}
+          {/* <Route path="/rating" component={ NewRatingContainer } />  */}
+          <Route path="/newrating" component={NewUserRatingContainer} />
 
-            {/* <Route path="/logout" render={ this.renderLogout } /> */}
-            <Route path="/allcomments" component={ AllCommentsContainer } /> 
-            {/* <Route path="/usersentiment" component={WatsonSentimentContainer}/> */}
-            {/* <Route path="/useremotions" component={WatsonEmotionContainer}/>  */}
-            <Route path="/charts" component={ChartsContainer}/>
-            <Route path="/analysis" component={AnalysisContainer}/> 
-            <Route path="/ratingsummary" component={RatingSummaryContainer}/> 
-            <Route path='/logout' render={this.handleLogout}/>
-            <Route path="/" exact render={() => <Home /> } />
-            <Route render={ () => <PageNotFound/> } />
-          </Switch>
+          {/* <Route path="/logout" render={ this.renderLogout } /> */}
+          <Route path="/allcomments" component={AllCommentsContainer} />
+          {/* <Route path="/usersentiment" component={WatsonSentimentContainer}/> */}
+          {/* <Route path="/useremotions" component={WatsonEmotionContainer}/>  */}
+          <Route path="/charts" component={ChartsContainer} />
+          <Route path="/analysis" component={AnalysisContainer} />
+          <Route path="/ratingsummary" component={RatingSummaryContainer} />
+          <Route path="/logout" render={this.handleLogout} />
+          <Route path="/" exact render={() => <Home />} />
+          <Route render={() => <PageNotFound />} />
+        </Switch>
         {/* </div> */}
       </Container>
     );
   }
-
 }
 // const mapStateToProps=(state) => {
 //   return{
@@ -184,4 +185,13 @@ class App extends React.Component {
 //   }
 // }
 
-export default connect(null, {clearallEmotionsFromState,clearSentimentFromState,clearUserFromState,addProductsToState, saveUserToState,addAllCommentsToState,addAllSentimentsToState,addAllEmotionsToState})(withRouter(App));
+export default connect(null, {
+  clearallEmotionsFromState,
+  clearSentimentFromState,
+  clearUserFromState,
+  addProductsToState,
+  saveUserToState,
+  addAllCommentsToState,
+  addAllSentimentsToState,
+  addAllEmotionsToState,
+})(withRouter(App));
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,68 +1,112 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom'
-import {Container} from 'semantic-ui-react'
-import {connect} from 'react-redux'
-import userReducer from '../Redux/userReducer';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+import { connect } from "react-redux";
+import userReducer from "../Redux/userReducer";
 
 const NavBar = (props) => {
-  return(
-    <Container>
+  return (
+    // <Container>
+    <nav>
+      <NavLink
+        to="#"
+        className="toggle-button"
+        onClick={() => {
+          document.querySelector("ul.nav").classList.toggle("active");
+        }}
+      >
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </NavLink>
       <ul className="nav">
         <li>
           <NavLink to="/">Home</NavLink>
         </li>
-      { !props.token?  <li>
-          <NavLink to="/login">Login</NavLink>
-        </li> : ""}
-        { !props.token ? <li>
-          <NavLink to="/signup">Signup</NavLink>
-        </li> : ""
-        }
+        {!props.token ? (
+          <li>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+        ) : (
+          ""
+        )}
+        {!props.token ? (
+          <li>
+            <NavLink to="/signup">Signup</NavLink>
+          </li>
+        ) : (
+          ""
+        )}
         <li>
           <NavLink to="/products">Products</NavLink>
         </li>
-          {/* {props.token ?  <li>
+        {/* {props.token ?  <li>
           <NavLink to="/rating">UserRating</NavLink>
         </li> : ""} */}
-        {props.token ?  <li>
-          <NavLink to="/newrating">Rating</NavLink>
-        </li> : ""}
-        {props.token && props.comments.length ? <li>
-          <NavLink to="/allcomments">AllComments</NavLink>
-        </li> :""}
+        {props.token ? (
+          <li>
+            <NavLink to="/newrating">Rating</NavLink>
+          </li>
+        ) : (
+          ""
+        )}
+        {props.token && props.comments.length ? (
+          <li>
+            <NavLink to="/allcomments">AllComments</NavLink>
+          </li>
+        ) : (
+          ""
+        )}
         {/* <li>
           <NavLink to="/usersentiment">UserSentiment</NavLink>
         </li> */}
-        {props.token && props.comments.length ? <li>
-          <NavLink to="/analysis">CommentAnalysis</NavLink>
-        </li> : ""}
-        {props.token && props.comments.length ? <li>
-          <NavLink to="/charts">Charts</NavLink>
-        </li> :""}
-        {props.token ? <li>
-          <NavLink to="/logout">Logout</NavLink>
-        </li> :""}
-        {props.token && (props.user.user_ratings.length===3) ? <li>
-          <NavLink to="/ratingsummary">RatingSummary</NavLink>
-        </li> : ""}
-
-
+        {props.token && props.comments.length ? (
+          <li>
+            <NavLink to="/analysis">CommentAnalysis</NavLink>
+          </li>
+        ) : (
+          ""
+        )}
+        {props.token && props.comments.length ? (
+          <li>
+            <NavLink to="/charts">Charts</NavLink>
+          </li>
+        ) : (
+          ""
+        )}
+        {props.token ? (
+          <li>
+            <NavLink to="/logout">Logout</NavLink>
+          </li>
+        ) : (
+          ""
+        )}
+        {props.token && props.user.user_ratings.length === 3 ? (
+          <li>
+            <NavLink to="/ratingsummary">RatingSummary</NavLink>
+          </li>
+        ) : (
+          ""
+        )}
       </ul>
-    </Container>
-  )
+    </nav>
+    // </Container>
+  );
 };
-const mapStateToProps =(state) => {
+const mapStateToProps = (state) => {
   return {
-    user:state.user.user,
-    token:state.user.token,
-    comments:state.comments.allcomments,
-    onerating:state.ratings.onerating
-  }
-}
+    user: state.user.user,
+    token: state.user.token,
+    comments: state.comments.allcomments,
+    onerating: state.ratings.onerating,
+  };
+};
 export default connect(mapStateToProps)(NavBar);
 
-{/* <div class="ui menu">
+{
+  /* <div class="ui menu">
   <a class="item">Editorials</a>
   <a class="item">Reviews</a>
   <a class="item">Upcoming Events</a>
-</div> */}
+</div> */
+}
