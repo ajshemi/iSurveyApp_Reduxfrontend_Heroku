e5085e2085442dde1f56f6288ca6c66ee49e7e83
ajshemi <akpojotor@yahoo.com>
2020-08-25 11:04:10 -0400

    add display error message

--- src/userRatingComponents/NewUserRatingContainer.js
+++ src/userRatingComponents/NewUserRatingContainer.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
-import Product from '../components/Product'
-import {connect} from 'react-redux'
-import {Grid, Card, GridColumn, Segment, Divider} from 'semantic-ui-react'
-import CommentInput from '../components/CommentInput'
-import Comments from '../components/Comments'
-import NewRatingContainer from './NewRatingContainer'
-import {saveUserToState} from '../Redux/actions'
-import ProductList from '../components/ProductList'
-
-
+import React, { Component } from "react";
+import Product from "../components/Product";
+import { connect } from "react-redux";
+import { Grid, Card, GridColumn, Segment, Divider } from "semantic-ui-react";
+import CommentInput from "../components/CommentInput";
+import Comments from "../components/Comments";
+import NewRatingContainer from "./NewRatingContainer";
+import { saveUserToState } from "../Redux/actions";
+import ProductList from "../components/ProductList";
 
 class NewUserRatingContainer extends Component {
-      //use the review_id to delete the review/rating/user association
-  handleDeleteReview=(review_id) => {
-    // if(this.props?.user){    
+  //use the review_id to delete the review/rating/user association
+  handleDeleteReview = (review_id) => {
+    // if(this.props?.user){
     //   // console.log(this.props.user)
     //   // console.log(review_id)
     //   fetch(`http://localhost:3000/reviews/${review_id}`, {
@@ -29,41 +27,47 @@ class NewUserRatingContainer extends Component {
     //   }
     //   )
     // }
-  }
-
+  };
 
   render() {
     //pass the app state as this.props.user
-    let {user} = this.props
-    // console.log(user)
-   return (
+    let { user, error } = this.props;
+    console.log(error);
+    return (
       <div className="newratingdiv">
         <div className="">
-        <h2>{user?.username ? `Welcome ${user.username}` : "Please login"}</h2>
-          <NewRatingContainer user={this.props.user}/>
+          <h2>
+            {user?.username ? `Welcome ${user.username}` : "Please login"}
+          </h2>
+          <NewRatingContainer user={this.props.user} />
         </div>
         <div className="">
-          <h3>{`You've Rated ${user.user_ratings.length < 1 ? 'No' : user.user_ratings.length } Product${user.user_ratings.length >1 ? 's':''}`}</h3>
-          {user.user_ratings.length < 1 ? "" : <ProductList/>}
+          <h3>{`You've Rated ${
+            user.user_ratings.length < 1 ? "No" : user.user_ratings.length
+          } Product${user.user_ratings.length > 1 ? "s" : ""}`}</h3>
+          {user.user_ratings.length < 1 ? "" : <ProductList />}
           {/* <Card.Group itemsPerRow={3}>
             {user?.user_ratings?.map(product => <Product handleDeleteReview={this.handleDeleteReview} key={product.review_id} product={product} user={user} />)}
           </Card.Group> */}
-        
         </div>
         <div className="addcomment">
           <CommentInput />
         </div>
+        <div>{Object.keys(error).length ? `Oops ${error}` : ""}</div>
         <div className="commentlist">
-          <Comments user={this.props.user}/>
+          <Comments user={this.props.user} />
         </div>
       </div>
-    )};
-  
+    );
+  }
 }
 const mapStateToProps = (state) => {
   return {
     user: state.user.user,
-    token:state.user.token
-  }
-}
-export default connect(mapStateToProps,{saveUserToState})(NewUserRatingContainer);
+    token: state.user.token,
+    error: state.error.errorMessage,
+  };
+};
+export default connect(mapStateToProps, { saveUserToState })(
+  NewUserRatingContainer
+);
